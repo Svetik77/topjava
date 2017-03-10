ec4a397984ecdcbfd1ba241da36909c815a78130
Svetik77 <rimma34@gmail.com>
2017-03-10 23:03:53 +0100

    using stream

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * GKislin
@@ -23,44 +24,47 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+
 
         List<UserMealWithExceed> mealWith = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 
+        Stream<UserMealWithExceed> secondStream = mealWith.stream();
 
-//        .toLocalDate();
-//        .toLocalTime();
+        secondStream.forEach(x -> System.out.println("date: " + x.getDateTime().getMonth() + " calory "+ x.getCalories()));
+
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         List<UserMealWithExceed> mealWithExceed = new ArrayList<>();
         Map<LocalDate, Integer> calloryPerDayMap = new HashMap<>();
 
-        for (UserMeal userMeal : mealList) {
-            LocalDate key = userMeal.getDateTime().toLocalDate();
+        Stream<UserMeal> firstStream = mealList.stream();
 
-
-            if (calloryPerDayMap.get(key) == null) {
-                calloryPerDayMap.put(key, new Integer(userMeal.getCalories()));
+        firstStream.forEach(x -> {
+            if (calloryPerDayMap.get(x.getDateTime().toLocalDate()) == null) {
+                calloryPerDayMap.put(x.getDateTime().toLocalDate(), x.getCalories());
             } else {
-                calloryPerDayMap.put(key, (calloryPerDayMap.get(key) + userMeal.getCalories()));
+                calloryPerDayMap.put(x.getDateTime().toLocalDate(),
+                        (calloryPerDayMap.get(x.getDateTime().toLocalDate()) + x.getCalories()));
             }
+        });
 
-        }
+        Stream<UserMeal> firstStream2 = mealList.stream();
 
-        for (UserMeal userMeal : mealList) {
+        firstStream2.forEach(x -> {
             boolean exceeded = false;
-            if (calloryPerDayMap.get(userMeal.getDateTime().toLocalDate()) > caloriesPerDay) {
+            if (calloryPerDayMap.get(x.getDateTime().toLocalDate()) > caloriesPerDay) {
                 exceeded = true;
             }
 
-            UserMealWithExceed userMealWithExceed = new UserMealWithExceed(userMeal.getDateTime(),
-                    userMeal.getDescription(), userMeal.getCalories(), exceeded);
-            if(TimeUtil.isBetween(userMeal.getDateTime().toLocalTime(), startTime, endTime)) {
+            UserMealWithExceed userMealWithExceed = new UserMealWithExceed(x.getDateTime(), x.getDescription(),
+                    x.getCalories(), exceeded);
+            if (TimeUtil.isBetween(x.getDateTime().toLocalTime(), startTime, endTime)) {
                 mealWithExceed.add(userMealWithExceed);
             }
+        });
 
-        }
+
         return mealWithExceed;
     }
 }
